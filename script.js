acdb55e0e93dacd0db190ea0a81e4331feda957e
kmaimn <kmai.mn@gmail.com>
2016-08-04 17:15:10 -0500

    updated and completed the assessment

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
 $(document).ready(function(){
-  $('#spies').append('<button class="addSpies">New Spy</button>');
-  $('.addSpies').on('click', addNewSpy);
-  $('#spies').append('<p>Total Number of Active Spies: ' + activeSpies + '</p>');
+  $('#spies').append('<button class="newSpy">Add Spy</button>' + 'Number of Spies: ' + '<span></span>');
+  $('.newSpy').on('click', addNewSpy);
+  $('#spies').on('click', '.delete', delSpy);
+  $('#spies').on('click', '.change', changeSpy);
+  $('.newSpy').on('click', spyCounter);
 });
-var activeSpies = 0;
-//add a new spy to the DOM, create a random number for spy;
+
+var spyCount = 0;
 
 function addNewSpy(){
-  $('#spies').append('<div class="spy"></div>');
-  $sp = $("#spies").children().last();
-  $sp.append('<p>Spy Number:'+ randomNumber(100, 500) + '<button class="change">Change</button>' + '<button class="delete">Delete</button>' + '</p>');
+  $('#spies').append('<div class="spy">' + 'Spy #: ' + randomNumber(100, 500) + '<button class="delete">Delete</button>' + '<button class="change">Change</button>' + '</div>');
 }
 
-
-//uses the random number function to randomly generate a spy number;
 function randomNumber(min, max){
     return Math.floor(Math.random() * (1 + max - min) + min);
 }
 
-//I couldn't get my button to work
-$(".spy").on("click", ".delete", delSpy)
-
 function delSpy(){
   $(this).parent().remove();
 }
+
+function changeSpy(){
+  $(this).parent().toggleClass('red');
+}
+
+function spyCounter(){
+  spyCount++;
+  console.log(spyCount);
+  $('span').text(spyCount);
+}
+
+//I referenced the weekend homework to help with the issue of my delete button not working - i had everythign in my document ready tags, so the position didnt matter in that case;
